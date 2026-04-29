a496fc1ae7f242f0110ac233f6545b4326f5ef76
Nizor <ogbezuodenizor@yahoo.com>
2026-04-29 14:14:08 +0100

    Updated inventory app and templates

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,7 +1,17 @@
+# inventory/forms.py
 from django import forms
-from .models import Product, Category
+from .models import Product
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'sku', 'barcode', 'category', 'price', 'cost', 'stock', 'reorder_level', 'is_active']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'sku': forms.TextInput(attrs={'class': 'form-control'}),
+            'barcode': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cost': forms.NumberInput(attrs={'class': 'form-control'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+            'reorder_level': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
